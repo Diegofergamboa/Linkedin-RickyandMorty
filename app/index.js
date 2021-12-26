6dbe5cb8e0bf7e672afb09d1d56a33529e464d73
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2021-12-26 18:19:47 -0500

    Importada API y performance del carge del nuevo personaje. Pendiente - Dar el set al cargar la página de nuevo -

--- app/index.js
+++ app/index.js
@@ -1,17 +1,25 @@
 /* Importar API */
 class API {
-    importData() {
-        fetch('https://rickandmortyapi.com/api/character/')
-            .then((response) => response.json())
-            .then((data) => {
-                getCharacter(data);
-            })
-            .catch(err => console.error(err));
+    async getCharacterById(id) {
+        const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
+        const data = await response.json()
+        return data
     }
 }
+const dataApi = new API()
 
-function getCharacter(data) {
-    console.log(data[1].info);
+/* Performance */
+
+function changeId(value) {
+    const randomCharacter = dataApi.getCharacterById(value);
+    return randomCharacter;
 }
 
+function pickRandom(min, max) {
+        return (Math.random() * (max - min) + min);
+}
+
+const newCharacter = changeId(parseInt(pickRandom(0, 827)));
+
+/* To save the performance work*/ 
 
