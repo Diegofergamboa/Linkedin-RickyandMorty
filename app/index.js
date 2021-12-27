efdab4e1b5c69c8939b86974ad0ebe323884dd36
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2021-12-27 17:16:46 -0500

    Consumiendo API

--- app/index.js
+++ app/index.js
@@ -3,12 +3,22 @@ class API {
     async getCharacterById(id) {
         const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
         const data = await response.json()
-        return data
+        const newData = response.console.log(data)
+
+        
+    //     data = JSON.parse(data);
+    //     const dataArray = []
+    //     for (let i ; i < data.length + 1 ; i++) {
+    //         data.push(data[i]);
+    //         console.log(data);
+    //     }
+    //     return dataArray
     }
 }
+
 const dataApi = new API()
 
-/* Performance */
+/* Performance of the random Character */
 
 function changeId(value) {
     const randomCharacter = dataApi.getCharacterById(value);
@@ -21,11 +31,13 @@ function pickRandom(min, max) {
 
 const newCharacter = changeId(parseInt(pickRandom(0, 827)));
 
-/* To save the performance work*/ 
+/* Calling the new Character with the button refresh */ 
 
 document.addEventListener("DOMContentLoaded" , () => {
-    setNewCharacter();
+    let btn = document.getElementById("btn-refresh");
+    btn.onclick = () => {
+        console.log(newCharacter);
+    } 
 })
 
 /* Setter of the new character */
-
