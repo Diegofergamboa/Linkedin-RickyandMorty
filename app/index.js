cff5635ffa48c58c15afcabcd7be4a1f5441c6d0
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2021-12-28 15:31:53 -0500

    Importadas las imagenes y, modificado el card del personaje

--- app/index.js
+++ app/index.js
@@ -7,26 +7,36 @@ async function getCharacter (id) {
     return data
 }
 
+
+
 async function get () {
     const nameCharacter = await getCharacter(100); // [1][1]
     // console.log(nameCharacter);
 
     /* ###CAMBIANDO LA INFORMACIÓN DEL PERSONAJES ### */ 
     // Cambiando el nombre
-    // let nameDom = document.getElementById('character-name');
-    // nameDom.innerHTML = nameCharacter[1][1];
+    let nameDom = document.getElementById('character-name');
+    nameDom.innerHTML = nameCharacter[1][1];
 
     // Cambiando character type [3][1]
-    // Cambiando character location [2][1]
-    // Cambiando el character info [5][1]
+    let typeDom = document.getElementById('character-type');
+    typeDom.innerHTML = 'Type: ' + nameCharacter[3][1];
 
-    //Setteando nuevas imagenes // // [10][1]
-    
-    let imageProfile = document.getElementById('profile.picture');
+    // Cambiando character location [2][1]
+    let locationDom = document.getElementById('character-location');
+    locationDom.innerHTML = 'Location: ' + nameCharacter[2][1];
+
+    // Cambiando el character info [5][1]
+    let infoDom = document.getElementById('character-info');
+    infoDom.innerHTML = 'Gender: ' + nameCharacter[5][1];
+
+    //Setteando nuevas imagenes // // 
+
+    let imageProfile = document.getElementById('profile-photo');
     let imageNav = document.getElementById('nav-image_content');
 
-    imageProfile.src= nameCharacter[10][1];
-    imageNav.src= nameCharacter[10][1];
+    imageProfile.src= `${nameCharacter[8][1]}`;
+    imageNav.src= `${nameCharacter[8][1]}`;
 
 }
 
