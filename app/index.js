d5cb68b495987109f56551a414b7ca34977b83df
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2021-12-28 13:55:14 -0500

    se consumio api - Trabajando en los cambios del HTML

--- app/index.js
+++ app/index.js
@@ -1,66 +1,24 @@
-/* Importar API */
-// class API {
-//     async getCharacterById(id) {
-//         const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
-//         const data = await response.json()
-//         const newData = ((data) => {
-//             const dataArray = Object.entries(newData)
-//             const dataCharacter = {
-//                 name : dataArray[1] ,
-//                 status : dataArray[2] ,
-//                 species : dataArray[3] ,
-//                 type : dataArray[4] ,
-//                 image : dataArray[8] ,
-//                 gender : dataArray[5] ,
-//             }
-//             return console.log(dataCharacter);        
-//         })
-//     }   
-// }
-class API {
-    getCharacterById(id) {
-        fetch(`https://rickandmortyapi.com/api/character/${id}`)
-            .then((response) => response.json())
-            .then((data) => {
-                const newData = data
-                const dataArray = Object.entries(newData)
-                const dataCharacter = {
-                    name : dataArray[1] ,
-                    status : dataArray[2] ,
-                    species : dataArray[3] ,
-                    type : dataArray[4] ,
-                    image : dataArray[8] ,
-                    gender : dataArray[5] ,
-                }
-                return dataCharacter        
-            })
-    }   
+
+async function getCharacter (id) {
+    const response = await fetch(`https://rickandmortyapi.com/api/character/${id}
+    `)
+    const json = await response.json()
+    const data = Object.entries(json);
+    return data
 }
 
-const dataApi = new API()
-console.log(dataApi.getCharacterById(1))
-/* Performance of the random Character */
+async function get () {
+    const nameCharacter = await getCharacter(1); // [1][1]
+    console.log(nameCharacter);
+    // Cambiando el nombre
+    // let nameDom = document.getElementById('character-name');
+    // nameDom.innerHTML = nameCharacter[1][1];
 
-// function changeId(value) {
-//     const randomCharacter = dataApi.getCharacterById(value);
-//     return randomCharacter;
-// }
+    // Cambiar character type
+    // Cambiar character location
+    // Cambiando el character info
 
-function pickRandom(min, max) {
-        return (Math.random() * (max - min) + min);
+
 }
 
-// const newCharacter = changeId(parseInt(pickRandom(0, 827)));
-
-/* Calling the new Character with the button refresh */ 
-
-document.addEventListener("DOMContentLoaded" , () => {
-    // const a = new API();
-    // async function setterCharacters() {
-    //     const setterCharacter = await getCharacter(parseInt(pickRandom(0, 827)));
-    //     const setterArrayCharacter = await gettinArray();
-    // }
-    // return setterCharacters(); 
-})
-
-/* Setter of the new character */
+get()
