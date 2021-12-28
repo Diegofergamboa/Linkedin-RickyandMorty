373f06a2a7a139e0b842b7404c772504913be8dd
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2021-12-28 14:18:48 -0500

    Importacion de modulos para cambiar imagenes e informacion del personaje

--- app/index.js
+++ app/index.js
@@ -8,16 +8,25 @@ async function getCharacter (id) {
 }
 
 async function get () {
-    const nameCharacter = await getCharacter(1); // [1][1]
-    console.log(nameCharacter);
+    const nameCharacter = await getCharacter(100); // [1][1]
+    // console.log(nameCharacter);
+
+    /* ###CAMBIANDO LA INFORMACIÓN DEL PERSONAJES ### */ 
     // Cambiando el nombre
     // let nameDom = document.getElementById('character-name');
     // nameDom.innerHTML = nameCharacter[1][1];
 
-    // Cambiar character type
-    // Cambiar character location
-    // Cambiando el character info
+    // Cambiando character type [3][1]
+    // Cambiando character location [2][1]
+    // Cambiando el character info [5][1]
 
+    //Setteando nuevas imagenes // // [10][1]
+    
+    let imageProfile = document.getElementById('profile.picture');
+    let imageNav = document.getElementById('nav-image_content');
+
+    imageProfile.src= nameCharacter[10][1];
+    imageNav.src= nameCharacter[10][1];
 
 }
 
