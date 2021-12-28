a685b712923d01558a7bfef57c4e76fbdd238f18
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2021-12-27 21:58:54 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,31 +1,44 @@
 /* Importar API */
+// class API {
+//     async getCharacterById(id) {
+//         const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
+//         const data = await response.json()
+//         const newData = ((data) => {
+//             const dataArray = Object.entries(newData)
+//             const dataCharacter = {
+//                 name : dataArray[1] ,
+//                 status : dataArray[2] ,
+//                 species : dataArray[3] ,
+//                 type : dataArray[4] ,
+//                 image : dataArray[8] ,
+//                 gender : dataArray[5] ,
+//             }
+//             return console.log(dataCharacter);        
+//         })
+//     }   
+// }
 class API {
-    async getCharacterById(id) {
-        const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
-        const data = await response.json()
-        const newData = await data;
-        await this.gettinArray(); 
-    }
-    async gettinArray(newData) {
-            const dataArray = Object.entries(newData)
-            const dataCharacter = {
-                name : dataArray[1] ,
-                status : dataArray[2] ,
-                species : dataArray[3] ,
-                type : dataArray[4] ,
-                image : dataArray[8] ,
-                gender : dataArray[5] ,
-            }
-            return dataCharacter;        
-        }
+    getCharacterById(id) {
+        fetch(`https://rickandmortyapi.com/api/character/${id}`)
+            .then((response) => response.json())
+            .then((data) => {
+                const newData = data
+                const dataArray = Object.entries(newData)
+                const dataCharacter = {
+                    name : dataArray[1] ,
+                    status : dataArray[2] ,
+                    species : dataArray[3] ,
+                    type : dataArray[4] ,
+                    image : dataArray[8] ,
+                    gender : dataArray[5] ,
+                }
+                return dataCharacter        
+            })
+    }   
 }
 
-const construction = ((a) => {
-    console.log('hello' + a)
-})
-
 const dataApi = new API()
-
+console.log(dataApi.getCharacterById(1))
 /* Performance of the random Character */
 
 // function changeId(value) {
@@ -42,11 +55,12 @@ function pickRandom(min, max) {
 /* Calling the new Character with the button refresh */ 
 
 document.addEventListener("DOMContentLoaded" , () => {
-    let btn = document.getElementById("btn-refresh");
-    btn.onclick = () => {
-        const setttt = (dataApi.getCharacterById(parseInt(pickRandom(0, 827))));
-        console.log(setttt);
-    }    
+    // const a = new API();
+    // async function setterCharacters() {
+    //     const setterCharacter = await getCharacter(parseInt(pickRandom(0, 827)));
+    //     const setterArrayCharacter = await gettinArray();
+    // }
+    // return setterCharacters(); 
 })
 
 /* Setter of the new character */
